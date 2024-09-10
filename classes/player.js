958d0bca78f4e2bd76337524a718d3d75fd7df69
JustALostSoul <135549816+JustALostSoul@users.noreply.github.com>
2024-09-10 19:27:40 +0000

    Added Player Inventory

--- classes/player.js
+++ classes/player.js
@@ -2,10 +2,19 @@ class Player{
     constructor(health = 4){
         this.health = health;
         this.nextPlayer = false;
+        this.itemInventory = [];
     }
 
     damage(damageValue = 1){
         this.health = this.health - damageValue;
+    }
+
+    addItem(item){
+        this.itemInventory.push(item);
+    }
+
+    removeItem(item){
+        this.itemInventory.splice(this.itemInventory.indexOf(item), 1);
     }
 }
 
